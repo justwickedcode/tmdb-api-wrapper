fdd6b826aa6d6c192b6a82ed28028c832b76e2f2
Die Narzisse <bulainarcis7@gmail.com>
2025-11-02 12:48:50 +0100

    Even more work on the types

--- src/sections/credits/types.ts
+++ src/sections/credits/types.ts
@@ -58,22 +58,29 @@ export interface CreditDetailsResponse {
 }
 
 export interface CastMember {
-  cast_id: number;
-  character: string;
-  credit_id: string;
-  gender: number | null;
-  id: number;
-  name: string;
-  order: number;
-  profile_path: string | null;
+  adult: boolean,
+  gender: number,
+  id: number,
+  known_for_department: string,
+  name: string,
+  original_name: string,
+  popularity: number,
+  profile_path: string | null,
+  character: string,
+  credit_id: string,
+  order: number
 }
 
 export interface CrewMember {
-  credit_id: string;
-  department: string;
-  gender: number | null;
-  id: number;
-  job: string;
-  name: string;
-  profile_path: string | null;
+  department: string,
+  job: string,
+  credit_id: string,
+  adult: boolean,
+  gender: number,
+  id: number,
+  known_for_department: string,
+  name: string,
+  original_name: string,
+  popularity: number,
+  profile_path: string | null
 }
