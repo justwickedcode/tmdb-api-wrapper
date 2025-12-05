dd3b485ad2a7e08726d7c93f531e8b879172b141
Just Wicked Code <bulainarcis7@gmail.com>
2025-12-05 20:36:58 +0100

    Tv left

--- src/client/services/person/person-service.ts
+++ src/client/services/person/person-service.ts
@@ -5,57 +5,57 @@ import { CustomRequestInit } from "../../types";
 export default class PersonService {
     constructor(private readonly defaultOptions: CustomRequestInit) {}
 
-    public async changesPeopleList(params?: ChangesPeopleListParams) {
+    public async getPeopleList(params?: ChangesPeopleListParams) {
         return await orval.changesPeopleList(params, this.defaultOptions);
     }
 
-    public async personPopularList(params?: PersonPopularListParams) {
+    public async getPopularList(params?: PersonPopularListParams) {
         return await orval.personPopularList(params, this.defaultOptions);
     }
 
-    public async personDetails(personId: number,
+    public async getDetails(personId: number,
         params?: PersonDetailsParams) {
         return await orval.personDetails(personId, params, this.defaultOptions);
     }
 
-    public async personChanges(personId: number,
+    public async getChanges(personId: number,
         params?: PersonChangesParams) {
         return await orval.personChanges(personId, params, this.defaultOptions);
     }
 
-    public async personCombinedCredits(personId: string,
+    public async getCombinedCredits(personId: string,
         params?: PersonCombinedCreditsParams) {
         return await orval.personCombinedCredits(personId, params, this.defaultOptions);
     }
 
-    public async personExternalIds(personId: number) {
+    public async getExternalIds(personId: number) {
         return await orval.personExternalIds(personId, this.defaultOptions);
     }
 
-    public async personImages(personId: number) {
+    public async getImages(personId: number) {
         return await orval.personImages(personId, this.defaultOptions);
     }
 
-    public async personLatestId() {
+    public async getLatestId() {
         return await orval.personLatestId(this.defaultOptions);
     }
 
-    public async personMovieCredits(personId: number,
+    public async getMovieCredits(personId: number,
         params?: PersonMovieCreditsParams) {
         return await orval.personMovieCredits(personId, params, this.defaultOptions);
     }
 
-    public async personTvCredits(personId: number,
+    public async getTvCredits(personId: number,
         params?: PersonTvCreditsParams) {
         return await orval.personTvCredits(personId, params, this.defaultOptions);
     }
 
-    public async personTaggedImages(personId: number,
+    public async getTaggedImages(personId: number,
         params?: PersonTaggedImagesParams) {
         return await orval.personTaggedImages(personId, params, this.defaultOptions);
     }
 
-    public async translations(personId: number) {
+    public async getTranslations(personId: number) {
         return await orval.translations(personId, this.defaultOptions);
     }
 }
